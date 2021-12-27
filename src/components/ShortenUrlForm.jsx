ddb0518b2ed611428257f07257199c72386c847c
Kenneth Chen <unusualkid@gmail.com>
2021-12-27 10:48:57 +0100

    refactor apicall to separate func

--- src/components/ShortenUrlForm.jsx
+++ src/components/ShortenUrlForm.jsx
@@ -7,9 +7,21 @@ const ShortenUrlForm = () => {
     const [shortUrl, setShortUrl] = useState(null);
     const apiEndpoint = 'https://api-ssl.bitly.com/v4/shorten';
 
+    const getBitLink = (url) => {
+        const data = {
+            "long_url": url
+        }
+        const axiosConfig = {
+            headers: {
+                'Authorization': `Bearer ${process.env.REACT_APP_BITLY_AUTHORIZATION_TOKEN}`
+            }
+        };
+        
+        return axios.post(apiEndpoint, data, axiosConfig)
+    }
+
     const onChange = useCallback(
         (e) => {
-
             setValue(e.target.value)
         },
         [setValue],
@@ -17,22 +29,14 @@ const ShortenUrlForm = () => {
 
     const onSubmit = useCallback(async (e) => {
             e.preventDefault();
-
             setShortUrl(null);
-            const data = {
-                "long_url": value
-            }
-            const axiosConfig = {
-                headers: {
-                    'Authorization': `Bearer ${process.env.REACT_APP_BITLY_AUTHORIZATION_TOKEN}`
-                }
-            };
-            
+
             try {
-                const result = await axios.post(apiEndpoint, data, axiosConfig)
-                setShortUrl(result.data.id);
-            } catch(error) {
-                console.log(error);
+                const res = await getBitLink(value);
+                setShortUrl(res.data.id);
+            }
+            catch (error) {
+                alert(`${error} while fetching the bitlink.`);
             }
         },
         [setShortUrl, value],
